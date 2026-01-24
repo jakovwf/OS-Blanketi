168eeb3dc9ee1355c9d4de2f0ef95a4f9b93bb76
jakovwf <jakovwf@gmail.com>
2026-01-24 19:29:07 +0100

    test commit

---
+++ Septembarski-2024/zad3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<wait.h>
+#include<sys/wait.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<string.h>
+
+
+int main(int argc,char*argv[]){
+    return 0;
+}
